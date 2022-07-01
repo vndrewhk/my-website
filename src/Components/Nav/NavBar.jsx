a4a4cc51967e6c124cb846ff7a9e05f014199967
Andrew Ma <andrewma@Andrews-MacBook-Pro.local>
2022-07-01 17:33:44 -0400

    added resume to page

--- src/Components/Nav/NavBar.jsx
+++ src/Components/Nav/NavBar.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route, Link, NavLink } from "react-router-dom";
 import "../../styling/css/NavBar.css";
+import resume from "../../assets/andrewhkma_resume.pdf";
 
 // hide on scroll down, show on scroll up
 // https://webdesign.tutsplus.com/tutorials/how-to-hide-reveal-a-sticky-header-on-scroll-with-javascript--cms-33756
@@ -14,12 +15,33 @@ const NavBar = () => {
         {/* <NavLink to="/about" title="About|Andrew Ma">
           <span className="hover-underline-animation">About</span>
         </NavLink> */}
-        <NavLink to="/resume">
-          <span className="hover-underline-animation">Resume</span>
-        </NavLink>
+        {/* <NavLink to="/resume"> */}
+        <a href={resume} target="_blank" className="hover-underline-animation">
+          Resume
+        </a>
+        {/* </NavLink> */}
+        {/* <a>Download Pdf</a> */}
       </div>
     </div>
   );
 };
 
 export default NavBar;
+
+// import React, { Component } from 'react';
+// import Pdf from '../Documents/Document.pdf';
+
+// class Download extends Component {
+
+//   render() {
+
+//     return (
+//         <div className = "App">
+//           <a href = {Pdf} target = "_blank">Download Pdf</a>
+//         </div>
+//     );
+
+//   }
+// }
+
+// export default Download;
